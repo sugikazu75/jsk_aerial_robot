1b9785d67e47ecfa91d8369a56178bda6b98e2e2
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2026-02-12 14:02:44 +0900

    [gimbalrotor][grape] add gimbal test script

--- robots/gimbalrotor/script/gimbal_test.py
+++ robots/gimbalrotor/script/gimbal_test.py
@@ -4,12 +4,12 @@ import rospy
 from sensor_msgs.msg import JointState
 import time
 
-rospy.init_node('zero_joint_0_1_2_3')
-pub = rospy.Publisher('/gimbalrotor/gimbals_ctrl', JointState, queue_size=1)
+rospy.init_node("zero_joint_0_1_2_3")
+pub = rospy.Publisher("/gimbalrotor/gimbals_ctrl", JointState, queue_size=1)
 radian = rospy.get_param("~radian", 0)
 
 msg = JointState()
-msg.name = ["gimbal1","gimbal2","gimbal3","gimbal4"]
+msg.name = ["gimbal1", "gimbal2", "gimbal3", "gimbal4"]
 msg.position = [radian, radian, radian, radian]
 
 time.sleep(0.6)
