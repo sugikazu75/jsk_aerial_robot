c1640a20ea93beee5838a57f8f63e1082accfce0
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2026-03-20 19:31:07 +0900

    [neuron] moved MPU9250 specific part to devrived class

---
+++ aerial_robot_nerve/neuron/neuronlib/IMU/drivers/mpu9250/mpu9250.h
@@ -0,0 +1,45 @@
+#ifndef APPLICATION_IMU_DRIVERS_MPU9250_MPU9250_H_
+#define APPLICATION_IMU_DRIVERS_MPU9250_MPU9250_H_
+
+#ifndef __cplusplus
+#error "Please define __cplusplus, because this is a c++ based file "
+#endif
+
+#include "IMU/imu_base.h"
+
+#define SPI_POLLING_MODE 0
+#define SPI_DMA_MODE 1
+#define SPI_MODE SPI_POLLING_MODE
+#define SENSOR_DATA_LENGTH 7
+
+#define IMU_SPI_CS_H       HAL_GPIO_WritePin(GPIOB,GPIO_PIN_6,GPIO_PIN_SET)
+#define IMU_SPI_CS_L      HAL_GPIO_WritePin(GPIOB,GPIO_PIN_6,GPIO_PIN_RESET)
+
+/* magenetometer update at about 100Hz, but the entire sensor data polling process is at 1KHz, should add prescaler for mag */
+#define MAG_PRESCALER 4
+/* magnetometer has bad noise bacause of the polling process, I think. So, we use a threshold filter method to remove the outlier */
+
+class MPU9250 : public IMU {
+public:
+  MPU9250(){}
+  MPU9250(uint8_t slave_id):IMU(slave_id){}
+  ~MPU9250(){}
+  void init(SPI_HandleTypeDef* hspi) override;
+  void update();
+
+  static  uint8_t adc_[SENSOR_DATA_LENGTH];
+
+private:
+  uint8_t dummy_[SENSOR_DATA_LENGTH];
+  bool ahb_tx_suspend_flag_; // to avoid the confliction between SPI1 and USART1TX(ros)
+
+  void gyroInit(void) override;
+  void accInit(void) override;
+  void magInit(void) override;
+  void pollingRead() override;
+
+  void mpuWrite(uint8_t address, uint8_t value);
+  uint8_t mpuRead(uint8_t address);
+};
+
+#endif /* APPLICATION_IMU_DRIVERS_MPU9250_MPU9250_H_ */
--- aerial_robot_nerve/neuron/neuronlib/IMU/imu_base.h
+++ aerial_robot_nerve/neuron/neuronlib/IMU/imu_base.h
@@ -1,5 +1,5 @@
 /*
- * imu_mpu9250.h
+ * imu_base.h
  *
  *  Created on: 2016/10/25
  *      Author: anzai
@@ -17,18 +17,6 @@
 
 using Vector3d = std::array<int16_t, 3>;
 
-#define SPI_POLLING_MODE 0
-#define SPI_DMA_MODE 1
-#define SPI_MODE SPI_POLLING_MODE
-#define SENSOR_DATA_LENGTH 7
-
-#define IMU_SPI_CS_H       HAL_GPIO_WritePin(GPIOB,GPIO_PIN_6,GPIO_PIN_SET)
-#define IMU_SPI_CS_L      HAL_GPIO_WritePin(GPIOB,GPIO_PIN_6,GPIO_PIN_RESET)
-
-/* magenetometer update at about 100Hz, but the entire sensor data polling process is at 1KHz, should add prescaler for mag */
-#define MAG_PRESCALER 4
-/* magnetometer has bad noise bacause of the polling process, I think. So, we use a threshold filter method to remove the outlier */
-
 class Initializer;
 
 class IMU : public CANDevice {
@@ -37,7 +25,7 @@ public:
   IMU(){}
   IMU(uint8_t slave_id):CANDevice(CAN::DEVICEID_IMU, slave_id){}
   ~IMU(){}
-  void init(SPI_HandleTypeDef* hspi);
+  virtual void init(SPI_HandleTypeDef* hspi);
   void update();
 
   bool getUpdate() { return update_; }
@@ -62,33 +50,23 @@ public:
   Vector3d  getGyro(){return gyro_;}
   Vector3d  getMag(){return mag_;}
 
-  static  uint8_t adc_[SENSOR_DATA_LENGTH];
-
   void sendData() override;
   void receiveDataCallback(uint8_t message_id, uint32_t DLC, uint8_t* data) override;
 
-private:
+protected:
 
   SPI_HandleTypeDef* hspi_;
 
   Vector3d acc_, gyro_, mag_;
 
-  uint8_t dummy_[SENSOR_DATA_LENGTH];
-
   bool update_;
-  bool ahb_tx_suspend_flag_; // to avoid the confliction between SPI1 and USART1TX(ros)
   uint16_t send_data_flag_;
 
-  void gyroInit(void);
-  void accInit(void);
-  void magInit(void);
+  virtual void gyroInit(void){};
+  virtual void accInit(void){};
+  virtual void magInit(void){};
 
-  void pollingRead (void);
-
-  void process (void);
-
-  void mpuWrite(uint8_t address, uint8_t value);
-  uint8_t mpuRead(uint8_t address);
+  virtual void pollingRead (void){};
 
   friend class Initializer;
 };
