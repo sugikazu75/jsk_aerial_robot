a0b181651dfc6caf7f0b37b40cf4e496b15b743b
sugikazu75 <sugikazu75@gmail.com>
2026-03-25 11:11:02 +0000

    Deploying to gh-pages from @ sugikazu75/jsk_aerial_robot@4a2d11c82dfc2db370f4d55b10bd0207f84b186a 🚀

--- search/all_8.js
+++ search/all_8.js
@@ -5,5 +5,6 @@ var searchData=
   ['imu_2',['Imu',['../classsensor__plugin_1_1Imu.html',1,'sensor_plugin']]],
   ['imuonboard_3',['IMUOnboard',['../classIMUOnboard.html',1,'']]],
   ['initgimbalangle_4',['InitGimbalAngle',['../classinit__gimbal__angles_1_1InitGimbalAngle.html',1,'init_gimbal_angles']]],
-  ['initializer_5',['Initializer',['../classInitializer.html',1,'']]]
+  ['initializer_5',['Initializer',['../classInitializer.html',1,'']]],
+  ['initpose_6',['InitPose',['../classgazebo__quick__init__pose_1_1InitPose.html',1,'gazebo_quick_init_pose']]]
 ];
--- search/classes_6.js
+++ search/classes_6.js
@@ -1,9 +1,12 @@
 var searchData=
 [
   ['gazebocontrolbridge_0',['GazeboControlBridge',['../classgazebo__control__bridge_1_1GazeboControlBridge.html',1,'gazebo_control_bridge']]],
-  ['gps_1',['GPS',['../classGPS.html',1,'']]],
-  ['gps_2',['Gps',['../classsensor__plugin_1_1Gps.html',1,'sensor_plugin']]],
-  ['gps_5fbackend_3',['GPS_Backend',['../classGPS__Backend.html',1,'']]],
-  ['gps_5fstate_4',['GPS_State',['../structGPS__State.html',1,'']]],
-  ['gps_5ftiming_5',['GPS_timing',['../structGPS__timing.html',1,'']]]
+  ['gimbalrotorcontroller_1',['GimbalrotorController',['../classaerial__robot__control_1_1GimbalrotorController.html',1,'aerial_robot_control']]],
+  ['gimbalrotornavigator_2',['GimbalrotorNavigator',['../classaerial__robot__navigation_1_1GimbalrotorNavigator.html',1,'aerial_robot_navigation']]],
+  ['gimbalrotorrobotmodel_3',['GimbalrotorRobotModel',['../classGimbalrotorRobotModel.html',1,'']]],
+  ['gps_4',['GPS',['../classGPS.html',1,'']]],
+  ['gps_5',['Gps',['../classsensor__plugin_1_1Gps.html',1,'sensor_plugin']]],
+  ['gps_5fbackend_6',['GPS_Backend',['../classGPS__Backend.html',1,'']]],
+  ['gps_5fstate_7',['GPS_State',['../structGPS__State.html',1,'']]],
+  ['gps_5ftiming_8',['GPS_timing',['../structGPS__timing.html',1,'']]]
 ];
