6c9bd84be63c485d3f865b89e8b6f642eb46eb3e
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2026-07-30 21:06:57 +0900

    [aerial_robot_dynamics] refactor test and benchmark build process

---
+++ aerial_robot_dynamics/test/test_utils.cpp
@@ -0,0 +1,28 @@
+#include <aerial_robot_dynamics/test_utils.h>
+#include <aerial_robot_dynamics/robot_model_ros.h>
+
+#include <ros/ros.h>
+
+namespace aerial_robot_dynamics
+{
+PinocchioRobotModel& getTestRobotModel()
+{
+  // ros::init() and the check for the robot description parameters are done by
+  // main(), so the model can be built on first use
+  ros::NodeHandle nh;
+  static PinocchioRobotModelRos robot_model_ros(nh);
+  return *robot_model_ros.getPinocchioRobotModel();
+}
+
+bool testVerbose()
+{
+  static const bool verbose = ros::NodeHandle("~").param("verbose", false);
+  return verbose;
+}
+
+int testTrials()
+{
+  static const int trials = ros::NodeHandle("~").param("trials", 3);
+  return trials;
+}
+}  // namespace aerial_robot_dynamics
